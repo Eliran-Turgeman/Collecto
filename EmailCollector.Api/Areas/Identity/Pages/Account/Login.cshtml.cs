e1bef2a33ca224fd0501e5f16e08c257510aecd6
Eliran <eliran9692@gmail.com>
2024-11-25 07:07:28 +0200

    unify css + home page

--- EmailCollector.Api/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ EmailCollector.Api/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -97,7 +97,7 @@ namespace EmailCollector.Api.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Forms/Index");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
