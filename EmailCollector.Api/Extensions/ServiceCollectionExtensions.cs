b6ab4e1dc6dc0e5a2058af8ecb69b6342488bca7
Eliran Turgeman <eliranturgeman@Elirans-MacBook-Air.local>
2024-12-15 21:04:14 +0200

    tidy program.cs

--- EmailCollector.Api/Extensions/ServiceCollectionExtensions.cs
+++ EmailCollector.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,21 @@
+using System.Reflection;
+using AspNetCoreRateLimit;
+using EmailCollector.Api.Authentication;
+using EmailCollector.Api.Configurations;
+using EmailCollector.Api.Data;
+using EmailCollector.Api.Middlewares;
+using EmailCollector.Api.Repositories;
+using EmailCollector.Api.Services;
+using EmailCollector.Api.Services.CustomEmailTemplates;
 using EmailCollector.Api.Services.CustomEmailTemplates.StorageProviders;
+using EmailCollector.Api.Services.EmailSender;
+using EmailCollector.Api.Services.Exports;
+using EmailCollector.Api.Services.Exports.ExportStrategies;
+using EmailCollector.Api.Services.Users;
+using EmailCollector.Domain.Entities;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.OpenApi.Models;
 
 namespace EmailCollector.Api.Extensions;
 
@@ -29,5 +46,170 @@ public static class ServiceCollectionExtensions
             default:
                 throw new InvalidOperationException($"Unsupported TemplateStorage provider: {storageProviderType}");
         }
+    }
+
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+         {
+             options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+             {
+                 In = ParameterLocation.Header,
+                 Name = "x-api-key",
+                 Type = SecuritySchemeType.ApiKey,
+                 Scheme = "ApiKeySchema"
+             });
+             var scheme = new OpenApiSecurityScheme
+             {
+                 Reference = new OpenApiReference
+                 {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "ApiKey"
+                 },
+                 In = ParameterLocation.Header
+             };
+             var requirement = new OpenApiSecurityRequirement
+             {
+                 { scheme, [] }
+             };
+             options.AddSecurityRequirement(requirement);
+             
+             options.SwaggerDoc("v1", new OpenApiInfo
+             {
+                 Version = "v1",
+                 Title = "Collecto",
+                 Description = "Email collection service",
+                 TermsOfService = new Uri("https://example.com/terms"),
+                 Contact = new OpenApiContact
+                 {
+                     Name = "Contact",
+                     Url = new Uri("https://github.com/Eliran-Turgeman/Collecto")
+                 },
+                 License = new OpenApiLicense
+                 {
+                     Name = "AGPL-3.0 License",
+                     Url = new Uri("https://github.com/Eliran-Turgeman/Collecto/blob/master/LICENSE")
+                 }
+             });
+         
+             var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+             options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+         });
+    }
+
+    public static void AddForms(this IServiceCollection services)
+    {
+        services.AddScoped<ISignupFormRepository, SignupFormRepository>();
+        services.AddScoped<IFormService, FormService>();
+        
+        services.AddScoped<IEmailSignupRepository, EmailSignupRepository>();
+        services.AddScoped<IEmailSignupService, EmailSignupService>();
+        
+        services.AddScoped(typeof(ExportMetadataResolver));
+        services.AddScoped<IExportService, ExportService>();
+        services.AddScoped<IExportFormatStrategy, CsvExportStrategy>();
+        services.AddScoped<IExportFormatStrategy, JsonExportStrategy>();
+
+        services.AddScoped<ICustomEmailTemplatesService, CustomEmailTemplatesService>();
+        services.AddScoped<IFormRelatedRepository<CustomEmailTemplate>, FormRelatedRepository<CustomEmailTemplate>>();
+
+    }
+
+    public static void AddApiKey(this IServiceCollection services)
+    {
+        services.AddScoped<IApiKeyRepository, ApiKeyRepository>();
+        services.AddScoped<IApiKeyService, ApiKeyService>();
+        
+        services.AddScoped<ApiKeyAuthFilter>();
+
+    }
+
+    public static void AddEmailSender(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        services.AddTransient<IAppEmailSender>(sp =>
+        {
+            // Resolve the IFeatureToggles service from the IServiceProvider
+            var featureToggles = sp.GetRequiredService<IFeatureToggles>();
+            var logger = sp.GetRequiredService<ILogger<EmailSender>>();
+            if (featureToggles.IsEmailConfirmationEnabled())
+            {
+                // Return the real EmailSender if the feature is enabled
+                var emailConfig = sp.GetService<EmailConfiguration>();
+                return new EmailSender(emailConfig!, logger);  // Assuming EmailSender depends on EmailConfiguration
+            }
+            else
+            {
+                // Return the NoOpEmailSender if the feature is disabled
+                return new NoOpAppEmailSender();
+            }
+        });
+        
+        // Register EmailConfiguration as a singleton if needed
+        var emailConfig = configuration
+            .GetSection("EmailConfiguration")
+            .Get<EmailConfiguration>();
+
+        if (emailConfig != null)
+        {
+            services.AddSingleton(emailConfig);
+        }
+    }
+
+    public static void AddCache(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = Environment.GetEnvironmentVariable("Redis__ConnectionString") ?? "redis:6379";
+        });
+    }
+
+    public static void AddRateLimiting(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        services.AddInMemoryRateLimiting();
+    }
+
+    public static void AddFeatureToggle(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<FeatureToggles>(configuration.GetSection("FeatureToggles"));
+        services.AddTransient<IFeatureToggles, FeatureTogglesService>();
+    }
+
+    public static void AddApiOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            var validDomains = configuration.GetSection("ValidCorsOrigins").Get<string>()?.Split(",") ?? [];
+            options.AddPolicy("AllowSpecificOrigin",
+                b => b.WithOrigins(validDomains)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+        
+        services.AddScoped<AllowedOriginsFilter>();
+        
+        services.AddIdentityApiEndpoints<EmailCollectorApiUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<EmailCollectorApiContext>();
+
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+
+                var activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+                context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+            };
+        });
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwagger();
+
     }
 }
--- EmailCollector.Api/Program.cs
+++ EmailCollector.Api/Program.cs
@@ -1,28 +1,14 @@
 using System.Net;
 using EmailCollector.Api.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using EmailCollector.Api.Middlewares;
-using EmailCollector.Api.Services;
 using EmailCollector.Api.Repositories;
-using System.Reflection;
 using Blazorise;
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using AspNetCoreRateLimit;
-using EmailCollector.Api.Authentication;
-using EmailCollector.Api.Services.EmailSender;
-using EmailCollector.Api.Configurations;
 using EmailCollector.Api.Extensions;
 using EmailCollector.Api.NotificationHandlers;
-using EmailCollector.Api.Services.CustomEmailTemplates;
-using EmailCollector.Api.Services.Exports;
-using EmailCollector.Api.Services.Exports.ExportStrategies;
-using EmailCollector.Api.Services.Users;
-using EmailCollector.Domain.Entities;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Identity.UI.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,60 +21,6 @@ builder.Services
     .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        // Serialize enums as strings
-        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
-    });
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Name = "x-api-key",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "ApiKeySchema"
-    });
-    var scheme = new OpenApiSecurityScheme
-    {
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "ApiKey"
-        },
-        In = ParameterLocation.Header
-    };
-    var requirement = new OpenApiSecurityRequirement
-    {
-        { scheme, [] }
-    };
-    options.AddSecurityRequirement(requirement);
-    
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Collecto",
-        Description = "Email collection service",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Contact",
-            Url = new Uri("https://github.com/Eliran-Turgeman/Collecto")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "AGPL-3.0 License",
-            Url = new Uri("https://github.com/Eliran-Turgeman/Collecto/blob/master/LICENSE")
-        }
-    });
-
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(SendEmailOnSignupHandler).Assembly));
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -98,39 +30,14 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.AccessDeniedPath = "/Identity/Account/AccessDenied";
     });
 
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-
-        var activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
-        context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
-    };
-});
-
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-builder.Services.AddScoped<ISignupFormRepository, SignupFormRepository>();
-builder.Services.AddScoped<IFormService, FormService>();
-
-builder.Services.AddScoped<IEmailSignupRepository, EmailSignupRepository>();
-builder.Services.AddScoped<IEmailSignupService, EmailSignupService>();
-
-builder.Services.AddScoped<IApiKeyRepository, ApiKeyRepository>();
-builder.Services.AddScoped<IApiKeyService, ApiKeyService>();
-
-builder.Services.AddScoped(typeof(ExportMetadataResolver));
-builder.Services.AddScoped<IExportService, ExportService>();
-builder.Services.AddScoped<IExportFormatStrategy, CsvExportStrategy>();
-builder.Services.AddScoped<IExportFormatStrategy, JsonExportStrategy>();
-
-builder.Services.AddScoped<ICustomEmailTemplatesService, CustomEmailTemplatesService>();
-builder.Services.AddScoped<IFormRelatedRepository<CustomEmailTemplate>, FormRelatedRepository<CustomEmailTemplate>>();
-
+builder.Services.AddForms();
+builder.Services.AddApiKey();
+builder.Services.AddEmailSender(builder.Configuration);
 builder.Services.AddTemplateStorageProvider(builder.Configuration);
-
+builder.Services.AddCache();
+builder.Services.AddRateLimiting(builder.Configuration);
+builder.Services.AddFeatureToggle(builder.Configuration);
+builder.Services.AddApiOptions(builder.Configuration);
 
 builder.Services.AddDbContext<EmailCollectorApiContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("EmailCollectorDB") ?? throw new InvalidOperationException("Connection string 'EmailCollectorDB' not found.")));
@@ -139,61 +46,7 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddHttpClient();
 
-builder.Services.AddMemoryCache();
-builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
-builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-builder.Services.AddInMemoryRateLimiting();
-
 builder.Services.AddAuthorization();
-
-builder.Services.AddIdentityApiEndpoints<EmailCollectorApiUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<EmailCollectorApiContext>();
-
-
-builder.Services.Configure<FeatureToggles>(builder.Configuration.GetSection("FeatureToggles"));
-builder.Services.AddTransient<IFeatureToggles, FeatureTogglesService>();
-
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-
-builder.Services.AddTransient<IAppEmailSender>(sp =>
-{
-    // Resolve the IFeatureToggles service from the IServiceProvider
-    var featureToggles = sp.GetRequiredService<IFeatureToggles>();
-    var logger = sp.GetRequiredService<ILogger<EmailSender>>();
-    if (featureToggles.IsEmailConfirmationEnabled())
-    {
-        // Return the real EmailSender if the feature is enabled
-        var emailConfig = sp.GetService<EmailConfiguration>();
-        return new EmailSender(emailConfig!, logger);  // Assuming EmailSender depends on EmailConfiguration
-    }
-    else
-    {
-        // Return the NoOpEmailSender if the feature is disabled
-        return new NoOpAppEmailSender();
-    }
-});
-
-// Register EmailConfiguration as a singleton if needed
-var emailConfig = builder.Configuration
-    .GetSection("EmailConfiguration")
-    .Get<EmailConfiguration>();
-
-if (emailConfig != null)
-{
-    builder.Services.AddSingleton(emailConfig);
-}
-
-builder.Services.AddCors(options =>
-{
-    var validDomains = builder.Configuration.GetSection("ValidCorsOrigins").Get<string>()?.Split(",") ?? [];
-    options.AddPolicy("AllowSpecificOrigin",
-        b => b.WithOrigins(validDomains)
-                              .AllowAnyMethod()
-                              .AllowAnyHeader());
-});
-
-builder.Services.AddScoped<ApiKeyAuthFilter>();
-builder.Services.AddScoped<AllowedOriginsFilter>();
 
 builder.Services.AddRazorPages(options =>
 {
@@ -203,14 +56,7 @@ builder.Services.AddRazorPages(options =>
     options.Conventions.AuthorizePage("/dashboard");
 });
 
-//added to use in-memory cache
-builder.Services.AddMemoryCache();
 
-//added to use Redis cache
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = Environment.GetEnvironmentVariable("Redis__ConnectionString") ?? "redis:6379";
-});
 
 var app = builder.Build();
 
